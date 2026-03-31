cf00ce46d87f09913c8fca43f29f3f19c59583f1
JackLeeo <130451237+JackLeeo@users.noreply.github.com>
2026-03-31 22:40:09 +0800

    Update title in rule object in fty.js

--- fty.js
+++ fty.js
@@ -1,10 +1,10 @@
 var rule = {
-  title: "饭太硬自托管",
-  host: "https://ghproxy.net/https://raw.githubusercontent.com/JackLeeo/tvbox-source/main/fty.json",
-  url: "",
-  searchUrl: "",
-  playUrl: "",
-  parse: function (html) {
+  "title": "自用聚合源",
+  "host": "https://ghproxy.net/https://raw.githubusercontent.com/JackLeeo/tvbox-source/main/fty.json",
+  "url": "",
+  "searchUrl": "",
+  "playUrl": "",
+  "parse": function (html) {
     return JSON.parse(html);
   }
 };
